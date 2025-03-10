891ef6d80618cd8010031f597b2a4f330d7948fd
eray0513 <eray@smith.edu>
2025-03-10 21:47:09 +0000

    post fix bugs

--- app/src/main/java/a4/Infix.java
+++ app/src/main/java/a4/Infix.java
@@ -5,7 +5,15 @@ import java.util.ArrayDeque;
 public class Infix {
 
     public static Double infixToPostfix(ArrayDeque<Object> tokens) {
-        
+        ArrayDeque<Object> output = new ArrayDeque<Object>();
+        while(!tokens.isEmpty()){
+            if(tokens.peekFirst() instanceof Double){
+                output.addFirst(tokens.removeFirst());
+            } else if(tokens.peekFirst() instanceof Character){
+                
+            }
+        }
+        return 0.0;
     }
 
 }
