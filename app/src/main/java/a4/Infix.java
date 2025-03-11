70f4c9d7c2ffdafe6dbcb5ec6e5a37fb9e26bb2f
eray0513 <eray@smith.edu>
2025-03-11 01:51:41 +0000

    infix first attempt

--- app/src/main/java/a4/Infix.java
+++ app/src/main/java/a4/Infix.java
@@ -6,14 +6,57 @@ public class Infix {
 
     public static Double infixToPostfix(ArrayDeque<Object> tokens) {
         ArrayDeque<Object> output = new ArrayDeque<Object>();
+        ArrayDeque<Character> stack = new ArrayDeque<Character>();
         while(!tokens.isEmpty()){
-            if(tokens.peekFirst() instanceof Double){
-                output.addFirst(tokens.removeFirst());
-            } else if(tokens.peekFirst() instanceof Character){
-                
+            Object current = tokens.removeFirst();
+            if(current instanceof Double){
+                output.addLast((Double)current);
+            } else if(current.equals('+')||current.equals('-')||current.equals('*')||current.equals('/')){
+                Character queueOp = (Character) current;
+                if(!stack.isEmpty()){
+                    int queueNum;
+                    int stackNum;
+                    if(queueOp.equals('+')||queueOp.equals('-')){
+                        queueNum = 1;
+                    } else {
+                        queueNum = 2;
+                    }
+                    if(stack.peekFirst().equals('+')||stack.peekFirst().equals('-')){
+                        stackNum = 1;
+                    } else{
+                        stackNum = 2;
+                    }
+                    while(queueNum<stackNum){
+                        output.add(stack.removeFirst());
+                        if(stack.peekFirst().equals('+')||stack.peekFirst().equals('-')){
+                            stackNum = 1;
+                        } else{
+                            stackNum = 2;
+                        }
+                    }
+                    stack.addFirst(queueOp);
+                }
+            } else if(current.equals('(')){
+                stack.add((Character) current);
+            } else if(current.equals(')')){
+                while(!stack.peekFirst().equals('(')){
+                    output.add(stack.removeFirst());
+                    if(stack.isEmpty()){
+                        throw new IllegalArgumentException("Mismatched parentheses.");
+                    }
+                }
+                stack.removeFirst();
             }
         }
-        return 0.0;
+        while(!stack.isEmpty()){
+            if(stack.peekFirst().equals('(')||stack.peekFirst().equals(')')){
+                throw new IllegalArgumentException("Mismatched parentheses.");
+            } else if(stack.peekFirst().equals('+')||stack.peekFirst().equals('-')||stack.peekFirst().equals('*')||stack.peekFirst().equals('/')){
+                output.addLast(stack.removeFirst());
+                }
+        }
+        Double result = Postfix.postfix(output);
+        return result;
     }
 
 }
